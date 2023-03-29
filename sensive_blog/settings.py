f8244cf34457cce0d92be07c2ce544e6a576da2a
Ilya <Dragoilya@yandex.ru>
2023-03-29 17:59:06 +0300

    Removed comments

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -94,11 +94,7 @@ USE_TZ = True
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
 
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
-
 MEDIA_ROOT = os.getenv("MEDIA_ROOT", os.path.join(BASE_DIR, 'media'))
-
-#STATIC_ROOT = os.getenv("STATIC_ROOT")
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 
